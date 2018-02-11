3150615aa04c802ea36476bf2c63e1afe9c80ca9
Bill Barnes <bill@billba-mbp.local>
2018-02-11 14:47:37 -0800

    demo

--- src/app.ts
+++ src/app.ts
@@ -1,118 +1,120 @@
-import { Bot, MemoryStorage, BotStateManager } from 'botbuilder-core';
+import { ConsoleAdapter } from 'botbuilder-node';
+import { Bot, Middleware, MemoryStorage, BotStateManager } from 'botbuilder';
 import { BotFrameworkAdapter } from 'botbuilder-services';
 import { createServer } from 'restify';
-import * as p from 'prague-fluent';
-import 'isomorphic-fetch';
-import { MatchRoute, MultipleRoute, TemplateRoute } from 'prague-fluent';
-import { race } from 'rxjs/operators/race';
 
-// Create server
-let server = createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`${server.name} listening to ${server.url}`);
-});
+const toUpper: Middleware = {
+    async postActivity(context, activities, postActivity) {
+        for (let activity of activities) {
+            if (activity.type === 'message')
+                activity.text = activity.text.toUpperCase();
+        }
 
-// Create connector
-const adapter = new BotFrameworkAdapter({ 
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-
-server.post('/api/messages', adapter.listen() as any);
-
-declare global {
-    interface ConversationState {
-        prompt?: string;
-        routes?: p.TemplateRoute[];
+        return postActivity();
     }
 }
 
-const regExp = (regExp: RegExp, c: BotContext) => () => regExp.exec(c.request.text);
-
-const templates = new p.Templates<
-    {
-        showTime: {
-            time: string
-        },
-        interviewMe: undefined,
-        greeting: string,
-        disambiguate: MultipleRoute,
-    },
-    BotContext
->(c => ({
-
-    showTime(time) {
-        c.reply(`The local time is ${time.time}`);
-    },
-
-    interviewMe() {
-        c.state.conversation.prompt = 'name';
-        return c.reply(`What's your name?`);
-    },
-
-    greeting(name) {
-        c.state.conversation.prompt = undefined;
-        return c.reply(`Nice to meet you, ${name}`);
-    },
-
-    disambiguate(route) {
-        c.state.conversation.routes = route.routes;
-        c.reply(`Are you telling me:`)
-        route.routes.forEach(route => {
-            c.reply(`${route.source}`);
-        });
-        c.state.conversation.prompt = 'disambiguate';
-    },
-
-}));
-
-const botLogic = (c: BotContext) => {
-    p.first(
-        () => {
-            if (c.request.type !== 'message')
-                return p.do(() => c.reply("Non-message activity"));
-        },
-        p.match(regExp(/interview me/, c),
-            templates.router('interviewMe')
-        ),
-        p.best(
-            () => {
-                switch (c.state.conversation.prompt) {
-                    case 'name': {
-                        return p.best(
-                            p.match(regExp(/I am (.*)/i, c),
-                                match => templates.route('greeting', match.value[1], "name", 1.0)
-                            ),
-                            p.match(regExp(/d+/, c),
-                                match => templates.route('greeting', match.value[1], "name", .1),
-                                templates.router('greeting', c.request.text, "name", .5),
-                            )
-                        )
-                    }
-
-                    case 'disambiguate': {
-                        const route = c.state.conversation.routes.find(route => route.source === c.request.text);
-                        if (route) {
-                            c.state.conversation.prompt = undefined;
-                            return new p.TemplateRoute(route.action, route.args);
-                        }
-                    }
-                }
-            },
-            p.match(regExp(/time/, c),
-                templates.router('showTime', { time: new Date().toLocaleTimeString() }, "time", .5)
-            )
-        )
-        .mapMultiple(route => templates.router('disambiguate', route))
-    )
-    .mapTemplate(templates, c)
-    .default(
-        p.do(() => c.reply(`I don't understand you humans.`))
-    )
-    .do();
+declare global {
+    interface ConversationState {
+        count: number;
+        knockknock: number;
+        prompt: string;  
+    }
 }
 
-const bot = new Bot(adapter)
+const adapter = new ConsoleAdapter();
+
+adapter.listen();
+
+const bot = new Bot(adapter);
+
+bot
     .use(new MemoryStorage())
     .use(new BotStateManager())
-    .onReceive(botLogic);
+    .use({
+        async receiveActivity(context, next) {
+            if (context.request.type === 'message') {
+                switch (context.state.conversation.knockknock) {
+                    case undefined: {
+                        if (context.request.text === 'knock knock') {
+                            context.reply(`Who's there?`);
+                            context.state.conversation.knockknock = 1;
+                        } else {
+                            await next();
+                        }
+                        return;
+                    }
+
+                    case 1: {
+                        context.reply(`${context.request.text} who?`);
+                        context.state.conversation.knockknock = 2;
+                        return;
+                    }
+
+                    case 2: {
+                        context.reply(`Hilarious!`);
+                        context.state.conversation.knockknock = undefined;
+                    }
+                }
+            }
+        }
+    })
+    .use({
+        async receiveActivity(context, next) {
+            context.state.conversation.count = context.state.conversation.count === undefined ? 0 : context.state.conversation.count + 1;
+            await next();
+        }
+    })
+    .use({
+        async receiveActivity(context, next) {
+            context.reply(`${context.state.conversation.count}: start`);
+            await next();
+            context.reply(`${context.state.conversation.count}: end`);
+        }
+    })
+    .use(toUpper)
+    .onReceive(context => {
+        context.reply('hello world');
+        if (context.request.type === 'message') {
+            switch(context.state.conversation.prompt) {
+                case 'name': {
+                    context.reply(`Nice to meet you, ${context.request.text}`);
+                    context.state.conversation.prompt = undefined;
+                }
+            }
+
+            if (context.request.text === 'name') {
+                context.reply(`What's your name?`);
+                context.state.conversation.prompt = 'name';
+            }
+
+            if (context.request.text === 'start') {
+                setInterval(
+                    () => {
+                        bot.createContext(context.conversationReference, c => {
+                            c.reply(`I am sending this in a different context`);
+                        })
+                    },
+                    3000
+                );
+            }                
+        }
+    });
+
+ /*
+    1. Bare adapter
+        * new ConsoleAdapter
+        * set onReceive
+        * listen
+    2. Introduce Bot and context
+        * responses.push, then reply
+        * state
+        * before/after
+    3. Proactive messages
+    4. Middleware
+        * state
+        * before/after
+        * toUpper (then move into functions)
+        * knock knock (state-driven)
+    5. Multi-instance state
+*/
