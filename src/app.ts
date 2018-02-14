6dd2a6de2ed3d5b020a9126bf9c4824e727f7398
Bill Barnes <bill@billba-mbp.guest.corp.microsoft.com>
2018-02-14 13:39:09 -0800

    Updated demo

--- src/app.ts
+++ src/app.ts
@@ -1,132 +1,119 @@
 import { ConsoleAdapter } from 'botbuilder-node';
-import { Bot, Middleware, MemoryStorage, BotStateManager } from 'botbuilder';
-import { BotFrameworkAdapter } from 'botbuilder-services';
+import { Bot, MemoryStorage, BotStateManager, Middleware } from 'botbuilder';
+// import { BotFrameworkAdapter } from 'botbuilder-services';
 import 'isomorphic-fetch';
-
-const toUpper: Middleware = {
-    async postActivity(context, activities, postActivity) {
-        for (let activity of activities) {
-            if (activity.type === 'message')
-                activity.text = activity.text.toUpperCase();
-        }
-
-        return postActivity();
-    }
-}
-
-declare global {
-    interface ConversationState {
-        count: number;
-        knockknock: number;
-        prompt: string;  
-    }
-}
+// import { createServer } from 'restify';
 
 const adapter = new ConsoleAdapter();
 
 adapter.listen();
 
+// const server = createServer();
+// server.listen(3978);
+// const adapter = new BotFrameworkAdapter();
+// server.post('api/messages', adapter.listen() as any);
+
 const bot = new Bot(adapter);
+
+const brackets: Middleware = {
+    async receiveActivity(context, next) {
+        context.reply(`{`);
+        await next();
+        context.reply(`}`);
+    }
+}
+
+declare global {
+    interface BotContext {
+        swName: (id: string) => Promise<string>;
+    }
+
+    interface ConversationState {
+        prompt: string;
+        knockknock: number;
+    }
+}
+
+const knockknock: Middleware = {
+    async receiveActivity(context, next) {
+        if (context.request.type === 'message') {
+            switch (context.state.conversation.knockknock) {
+                case undefined: {
+                    if (context.request.text === 'knock knock') {
+                        context.reply(`Who's there?`);
+                        context.state.conversation.knockknock = 1;
+                    } else {
+                        await next();
+                    }
+                    return;
+                }
+
+                case 1: {
+                    context.reply(`${context.request.text} who?`);
+                    context.state.conversation.knockknock = 2;
+                    return;
+                }
+
+                case 2: {
+                    context.reply(`Hilarious!`);
+                    context.state.conversation.knockknock = undefined;
+                    return;
+                }
+            }
+        }
+    }
+}
 
 bot
     .use(new MemoryStorage())
     .use(new BotStateManager())
+    .use(brackets)
     .use({
-        async receiveActivity(context, next) {
-            if (context.request.type === 'message') {
-                switch (context.state.conversation.knockknock) {
-                    case undefined: {
-                        if (context.request.text === 'knock knock') {
-                            context.reply(`Who's there?`);
-                            context.state.conversation.knockknock = 1;
-                        } else {
-                            await next();
-                        }
-                        return;
-                    }
+        async contextCreated(context, next) {
+            context.swName = (id: string) => fetch(`https://swapi.co/api/people/${id}`)
+                .then(res => res.json())
+                .then(json => json.name);
 
-                    case 1: {
-                        context.reply(`${context.request.text} who?`);
-                        context.state.conversation.knockknock = 2;
-                        return;
-                    }
-
-                    case 2: {
-                        context.reply(`Hilarious!`);
-                        context.state.conversation.knockknock = undefined;
-                    }
-                }
+            await next();
+        }
+    })
+    .use(knockknock)
+    .use({
+        async postActivity(context, activities, post) {
+            for (let activity of activities) {
+                if (activity.type === 'message')
+                    activity.text = activity.text.toUpperCase();
             }
+
+            return post();
         }
     })
-    .use({
-        async receiveActivity(context, next) {
-            context.state.conversation.count = context.state.conversation.count === undefined ? 0 : context.state.conversation.count + 1;
-            await next();
-        }
-    })
-    .use({
-        async receiveActivity(context, next) {
-            context.reply(`{ ${context.state.conversation.count}`);
-            await next();
-            context.reply(`}`);
-        }
-    })
-    .use(toUpper)
-    .onReceive(context => {
-        context.reply('hello world');
+    .onReceive(async context => {
         if (context.request.type === 'message') {
             const text = context.request.text;
 
             const matches = /star wars (\d+)/.exec(text);
-            if (matches)
-                return fetch(`https://swapi.co/api/people/${matches[1]}`)
-                    .then(res => res.json())
-                    .then(json => json.name)
-                    .then(name => {
-                        context.reply(name);
-                    });
+            if (matches) {
+                context.reply(await context.swName(matches[1]));
+                return;
+            }
 
-            switch(context.state.conversation.prompt) {
+            if (text === 'interview me') {
+                context.reply(`What's your name?`);
+                context.state.conversation.prompt = 'name';
+                return;
+            }
+
+            switch (context.state.conversation.prompt) {
                 case 'name': {
-                    context.reply(`Nice to meet you, ${context.request.text}`);
+                    context.reply(`Nice to meet you`);
                     context.state.conversation.prompt = undefined;
+                    return;
                 }
             }
 
-            if (context.request.text === 'name') {
-                context.reply(`What's your name?`);
-                context.state.conversation.prompt = 'name';
-            }
-
-            if (context.request.text === 'start') {
-                setInterval(
-                    () => {
-                        bot.createContext(context.conversationReference, c => {
-                            c.reply(`I am sending this in a different context`);
-                        })
-                    },
-                    3000
-                );
-            }                
+            context.reply(`I don't understand you humans.`);
         }
     });
 
- /*
-    1. Bare adapter
-        * new ConsoleAdapter
-        * set onReceive
-        * listen
-    2. Introduce Bot and context
-        * responses.push, then reply
-        * fetch (promises)
-        * simple prompt
-        * state => multi-instance state
-        * count, before/after
-    3. Proactive messages
-    4. Middleware
-        * state
-        * before/after
-        * toUpper (then move into functions)
-        * knock knock (state-driven)
-*/
+    
