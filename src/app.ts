16a6d8800edd5bea6de0e236c50af00ea5526a93
Bill Barnes <bill@billba-mbp.local>
2018-02-25 17:56:04 +0100

    so many improvements!

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { Bot, MemoryStorage, BotStateManager, Middleware } from 'botbuilder';
 // import { BotFrameworkAdapter } from 'botbuilder-services';
 import 'isomorphic-fetch';
 // import { createServer } from 'restify';
-import { Topic, TopicWithChild } from './topics';
+import { Topic } from './topics';
 
 const adapter = new ConsoleAdapter();
 
@@ -19,10 +19,8 @@ bot
             return await next();
         }
     })
-    .onReceive(async context => {
-        if (context.request.type === 'message') {
-
-        }
+    .onReceive(context => {
+        Topic.do(context, () => alarmBot.createInstance(context));
     });
 
 interface Alarm {
@@ -31,38 +29,78 @@ interface Alarm {
 }
 
 interface AlarmBot {
+    child: string;
     alarms: Alarm[];
 }
 
-const addAlarm = new Topic<Partial<Alarm>, Partial<Alarm>>('addAlarm', {
-    init (context, instance, args) {
-        instance.state = args;
+const setAlarm = new Topic<Partial<Alarm>, Partial<Alarm>, Alarm>('addAlarm', {
+    init (context, topic) {
+        topic.instance.state = topic.args;
+        context.reply(`Let's fake setting an alarm. Type "done".`);
+    },
+
+    async onReceive (context, topic) {
+        if (context.request.type === 'message') {
+            if (context.request.text === 'done') {
+                topic.complete({
+                    name: "dog",
+                    when: new Date(),
+                });
+            }  else {
+                context.reply(`I didn't understand that.`);
+            }
+        }
     }
 });
 
-const alarmBot = new TopicWithChild<AlarmBot>('alarmbot', {
-    init (context, instance) {
+const showAlarms = new Topic<undefined, { alarms: Alarm[] }>('showAlarms', {
+    init (context, topic) {
+        context.reply(`You have the following alarms set:`);
+        topic.args.alarms.forEach(alarm => context.reply(`${alarm.name} for ${alarm.when}`));
+        topic.complete();
+    }
+});
+
+const alarmBot = new Topic<AlarmBot>('alarmbot', {
+    init (context, topic) {
         context.reply(`Welcome to Alarm Bot! I know how to set, show, and delete alarms.`);
-        instance.state.alarms = [];
+        topic.instance.state.child = undefined;
+        topic.instance.state.alarms = [];
     },
 
-    async onReceive (context, instance) {
-        if (instance.child)
-            return Topic.onReceive(context, instance.child);
+    async onReceive (context, topic) {
+        if (topic.instance.state.child)
+            return Topic.dispatchToInstance(context, topic.instance.state.child);
         else if (context.request.type === 'message') {
-            if (context.request.text.includes("add alarm")) {
-                instance.child = await addAlarm.createInstance(context);
+            if (context.request.text === "set alarm") {
+                topic.instance.state.child = await setAlarm.createInstance(context, {}, {
+                    instanceName: topic.instance.name,
+                    tag: 'addAlarm'
+                });
+            } else if (context.request.text === "show alarms") {
+                topic.instance.state.child = await showAlarms.createInstance(context, {
+                    alarms: topic.instance.state.alarms
+                });
+            } else {
+                context.reply(`I didn't understand that.`);
             }
         }
     },
 
-    async callback (context, response, instance) {
+    callback (context, instance, args, tag, child) {
+        switch (tag) {
+            case 'addAlarm': {
+                if (args) {
+                    instance.state.alarms.push(args);
+                    context.reply(`Alarm successfully added!`);
+                    instance.state.child = undefined;
+                    return;
+                }
+            }
 
+            default:
+                throw "no such tag";
+        }
     }
 
-
 });
-
-let context: BotContext;
-
-Topic.setRoot(context, Topic.createInstance(context, profile))
--- src/topics.ts
+++ src/topics.ts
@@ -1,18 +1,27 @@
 import { Promiseable } from 'botbuilder';
 
-export const toPromise = <T> (t: Promiseable<T>) => t instanceof Promise ? t : Promise.resolve(t);
+const toPromise = <T> (t: Promiseable<T>) => t instanceof Promise ? t : Promise.resolve(t);
+
+interface Callback {
+    instanceName: string,
+    tag: string,
+}
 
 class TopicInstance <State = any> {
+    public name: string;
+    public state = {} as State;
+
     constructor(
         public topicName: string,
-        public state = {} as State,
+        public callback?: Callback,
     ) {
     }
+
 }
 
 declare global {
     interface ConversationState {
-        topics: {
+        topical: {
             instances: {
                 [instanceName: string]: TopicInstance;
             }
@@ -21,137 +30,195 @@ declare global {
     }
 }
 
+interface TopicInitHelperData <CallbackArgs> {
+    complete: boolean;
+    args: CallbackArgs;
+    dispatch: boolean;
+}
+
+class TopicInitHelper <
+    State,
+    InitArgs,
+    CallbackArgs,
+    Instance extends TopicInstance<State>,
+> {
+    constructor(
+        private context: BotContext,
+        public instance: Instance,
+        public args: InitArgs,
+        private data: TopicInitHelperData <CallbackArgs>
+    ) {
+    }
+
+    complete (
+        args?: CallbackArgs,
+    ) {
+        if (this.data.dispatch)
+            throw "you may call complete() or dispatch() but not both";
+
+        this.data.complete = true;
+        this.data.args = args;
+    }
+
+    dispatch () {
+        if (this.data.complete)
+            throw "you may call complete() or dispatch() but not both";
+        this.data.dispatch = true;
+    }
+}
+
 type TopicInit <
-    State = any,
-    InitArgs = any,
-    Instance extends TopicInstance<State> = TopicInstance<State>,
+    State,
+    InitArgs,
+    CallbackArgs,
+    Instance extends TopicInstance<State>,
+    T
 > = (
     context: BotContext,
-    instance: Instance,
-    args: InitArgs,
-) => Promiseable<void>;
+    topic: TopicInitHelper<State, InitArgs, CallbackArgs, Instance>,
+) => T;
 
-type NormalizedTopicInit <
-    State = any,
-    InitArgs = any,
-    Instance extends TopicInstance<State> = TopicInstance<State>,
-> = (
-    context: BotContext,
-    instance: Instance,
-    args: InitArgs,
-) => Promise<void>;
 
-const normalizedTopicInit = <
-    State = any,
-    InitArgs = any,
-    Instance extends TopicInstance<State> = TopicInstance<State>,
-> (
-    init: TopicInit<State, InitArgs, Instance>
-): NormalizedTopicInit<State, InitArgs, Instance> => (
-    context,
-    instance,
-    args
-) => init
-    ? toPromise(init(context, instance, args))
-    : Promise.resolve();
+interface TopicOnReceiveHelperData <CallbackArgs> {
+    complete: boolean;
+    args: CallbackArgs
+}
+
+class TopicOnReceiveHelper <
+    State,
+    CallbackArgs,
+    Instance extends TopicInstance<State>,
+> {
+    constructor(
+        private context: BotContext,
+        public instance: Instance,
+        private data: TopicOnReceiveHelperData<CallbackArgs>,
+    ) {
+    }
+
+    complete (
+        args: CallbackArgs,
+    ) {
+        if (!this.instance.callback)
+            throw "you need to provide a callback before calling complete()";
+        this.data.complete = true;
+        this.data.args = args;
+    }
+}
 
 type TopicOnReceive <
-    State = any,
-    Instance extends TopicInstance<State> = TopicInstance<State>,
+    State,
+    CallbackArgs,
+    Instance extends TopicInstance<State>,
+    T
+> = (
+    context: BotContext,
+    topic: TopicOnReceiveHelper<State, CallbackArgs, Instance>
+) => T;
+
+type TopicCallback <
+    State,
+    Instance extends TopicInstance<State>,
+    CallbackArgs,
+    T
 > = (
     context: BotContext,
     instance: Instance,
-) => Promiseable<void>;
-
-type NormalizedTopicOnReceive <
-    State = any,
-    Instance extends TopicInstance<State> = TopicInstance<State>,
-> = (
-    context: BotContext,
-    instance: Instance,
-) => Promise<void>;
-
-const normalizedTopicOnReceive = <
-    State = any,
-    Instance extends TopicInstance<State> = TopicInstance<State>,
-> (
-    onReceive: TopicOnReceive<State, Instance>
-): NormalizedTopicOnReceive<State, Instance> =>
-(
-    context,
-    instance
-) => onReceive
-    ? toPromise(onReceive(context, instance))
-    : Promise.resolve();
+    args: CallbackArgs,
+    tag: string,
+    childInstanceName: string,
+) => T;
 
 interface TopicMethods <
-    State = any,
-    InitArgs = any,
-    Instance extends TopicInstance<State> = TopicInstance<State>,
+    State,
+    InitArgs,
+    CallbackArgs,
+    Instance extends TopicInstance<State>,
 > {
-    init: TopicInit<State, InitArgs, Instance>;
-    onReceive: TopicOnReceive<State, Instance>;
+    init: TopicInit<State, InitArgs, CallbackArgs, Instance, Promiseable<void>>;
+    onReceive: TopicOnReceive<State, CallbackArgs, Instance, Promiseable<void>>;
+    callback: TopicCallback<State, Instance, CallbackArgs, Promiseable<void>>;
 }
 
 export class Topic <
     State = any,
     InitArgs = any,
+    CallbackArgs = any,
 > {
     private static topics: {
         [name: string]: Topic;
-    }
+    } = {}
 
-    protected init: NormalizedTopicInit<State, InitArgs>;
-    protected onReceive: NormalizedTopicOnReceive<State>;
+    protected init: TopicInit<State, InitArgs, CallbackArgs, TopicInstance<State>, Promise<void>>;
+    protected onReceive: TopicOnReceive<State, CallbackArgs, TopicInstance<State>, Promise<void>>;
+    protected callback: TopicCallback<State, TopicInstance<State>, CallbackArgs, Promise<void>>;
 
     constructor (
         public name: string,
-        topicMethods: Partial<TopicMethods<State, InitArgs, TopicInstance<State>>>,
+        topicMethods: Partial<TopicMethods<State, InitArgs, CallbackArgs, TopicInstance<State>>>,
     ) {
         if (Topic.topics[name])
             throw new Error(`An attempt was made to create a topic with existing name ${name}. Ignored.`);
         
-        this.init = normalizedTopicInit(topicMethods.init);
-        this.onReceive = normalizedTopicOnReceive(topicMethods.onReceive);
+        this.init = (context, topic) => toPromise((topicMethods.init || (() => {}))(context, topic));
+        this.onReceive = (context, instance) => toPromise((topicMethods.onReceive || (() => {}))(context, instance));
+        this.callback = (context, instance, args, tag, childInstanceName) => toPromise((topicMethods.callback || (() => {}))(context, instance, args, tag, childInstanceName));
 
         Topic.topics[name] = this;
     }
 
-    protected async saveInstance (
+    protected saveInstance (
         context: BotContext,
         instance: TopicInstance<State>,
-        args?: InitArgs,
     ) {
-        await toPromise(this.init(context, instance, args));
-
-        const instanceName = Date.now().toString();
-        context.state.conversation.topics.instances[instanceName] = instance;
-        return instanceName;
+        instance.name = Date.now().toString();
+        context.state.conversation.topical.instances[instance.name] = instance;
+        return instance.name;
     }
 
-    createInstance (
+    async createInstance (
         context: BotContext,
         args?: InitArgs,
+        callback?: Callback, 
     ) {
-        return this.saveInstance(
-            context,
-            new TopicInstance(this.name),
-            args,
-        );
+        const data = {} as TopicInitHelperData<CallbackArgs>;
+        const instance = new TopicInstance<State>(this.name, callback);
+
+        await toPromise(this.init(context, new TopicInitHelper(context, instance, args, data)));
+        if (data.complete) {
+            if (instance.callback)
+                await Topic.completeInstance(context, instance, data.args);
+            return undefined;
+        } else {
+            const instanceName = this.saveInstance(context, instance);
+            if (data.dispatch) {
+                await Topic.dispatchToInstance(context, instanceName);
+            }
+            return instanceName;
+        }
     }
 
-    static setRoot (
+    static async do(
         context: BotContext,
-        instanceName: string,
+        getRootInstanceName: () => Promise<string>
     ) {
-        context.state.conversation.topics.rootInstanceName = instanceName;
+        if (!context.state.conversation.topical) {
+            context.state.conversation.topical = {
+                instances: {},
+                rootInstanceName: undefined
+            }
+
+            context.state.conversation.topical.rootInstanceName = await getRootInstanceName();
+        } else {
+            await Topic.dispatchToInstance(context, context.state.conversation.topical.rootInstanceName);
+        }
     }
 
-    static dispatchToInstance (
+    static async dispatchToInstance (
         context: BotContext,
         instanceName: string,
     ): Promise<void> {
-        const instance = context.state.conversation.topics.instances[instanceName];
+        const instance = context.state.conversation.topical.instances[instanceName];
 
         if (!instance) {
             console.warn(`Unknown instance ${instanceName}`);
@@ -165,130 +232,53 @@ export class Topic <
             return Promise.resolve();
         }
 
-        return topic.onReceive(context, instance);
+        const data = {} as TopicOnReceiveHelperData<any>;
+
+        await topic.onReceive(context, new TopicOnReceiveHelper(context, instance, data));
+
+        if (data.complete) {
+            await Topic.completeInstance(context, instance, data.args);
+        }
     }
-}
 
-type TopicCallback <
-    State = any,
-    Response = any,
-> = (
-    context: BotContext,
-    response: Response,
-    instance: TopicInstance<State>,
-) => Promiseable<void>;
-
-type NormalizedTopicCallback <
-    State = any,
-    Response = any,
-> = (
-    context: BotContext,
-    response: Response,
-    instance: TopicInstance<State>,
-) => Promise<void>;
-
-const normalizedTopicCallback = <
-    State = any,
-    Response = any,
-> (
-    callback: TopicCallback<State, Response>
-): NormalizedTopicCallback<State, Response> => (
-    context,
-    response,
-    instance,
-) => callback
-    ? toPromise(callback(context, response, instance))
-    : Promise.resolve();
-
-interface TopicMethodsWithCallback <
-    State = any,
-    InitArgs = any,
-    Response = any,
-    Instance extends TopicInstance<State> = TopicInstance<State>,
-> extends TopicMethods<State, InitArgs, Instance> {
-    callback: TopicCallback<State, Response>;
-}
-
-export abstract class TopicWithCallbacks <
-    State = any,
-    InitArgs = any,
-    Response = any,
-> extends Topic<State, InitArgs> {
-    protected callback: TopicCallback<State, Response>;
-
-    constructor (
-        name: string,
-        topicMethods: Partial<TopicMethodsWithCallback<State, InitArgs, Response>>,
+    static completeInstance <CallbackArgs = any> (
+        context: BotContext,
+        instance: TopicInstance<any>,
+        args: CallbackArgs,
     ) {
-        super(name, topicMethods);
-        this.callback = normalizedTopicCallback(topicMethods.callback);
+        const callback = instance.callback;
+
+        if (!callback) {
+            console.warn(`No callback registered for instance ${instance.name}`);
+            return Promise.resolve();
+        }
+
+        const parentInstance = context.state.conversation.topical.instances[callback.instanceName];
+
+        if (!parentInstance) {
+            console.warn(`Unknown parent instance ${callback.instanceName}`);
+            return Promise.resolve();
+        }
+
+        const topic = Topic.topics[parentInstance.topicName];
+        
+        if (!topic) {
+            console.warn(`Unknown topic ${parentInstance.topicName}`);
+            return Promise.resolve();
+        }
+
+        return topic.callback(context, parentInstance, args, callback.tag, instance.name);
     }
 }
 
-class TopicInstanceWithChild <State = any> extends TopicInstance<State> {
+
+class ParentTopicInstance <State = any> extends TopicInstance <State> {
+    public children = [] as string[];
+
     constructor(
         topicName: string,
-        state = {} as State,
-        public child = undefined as string,
+        callback?: Callback,
     ) {
-        super(topicName, state);
-    }
-}
-
-export class TopicWithChild <
-    State = any,
-    InitArgs = any,
-    Response = any,
-> extends TopicWithCallbacks<State, InitArgs, Response> {
-    constructor (
-        public name: string,
-        topicMethods: Partial<TopicMethodsWithCallback<State, InitArgs, Response, TopicInstanceWithChild<State>>>,
-    ) {
-        super (name, topicMethods);
-    }
-
-    createInstance (
-        context: BotContext,
-        args?: InitArgs,
-    ) {
-        return this.saveInstance(
-            context,
-            new TopicInstanceWithChild(this.name),
-            args,
-        );
-    }
-}
-
-class TopicInstanceWithChildren <State = any> extends TopicInstance <State> {
-    constructor(
-        topicName: string,
-        state = {} as State,
-        public children = [] as string[],
-    ) {
-        super(topicName, state);
-    }
-}
-
-export class TopicWithChildren <
-    State = any,
-    InitArgs = any,
-    Response = any,
-> extends TopicWithCallbacks <State, InitArgs, Response> {
-    constructor (
-        public name: string,
-        topicMethods: Partial<TopicMethodsWithCallback<State, InitArgs, Response, TopicInstanceWithChildren<State>>>,
-    ) {
-        super (name, topicMethods);
-    }
-
-    createInstance (
-        context: BotContext,
-        args?: InitArgs,
-    ) {
-        return this.saveInstance(
-            context,
-            new TopicInstanceWithChildren(this.name),
-            args,
-        );
+        super(topicName, callback);
     }
 }
