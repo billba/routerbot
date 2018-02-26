cdaf29acb7ddc58136bf97b4c3213b2d5c2f5bc7
Bill Barnes <bill@billba-mbp.local>
2018-02-26 21:21:58 +0100

    Alarmbot featuring Topical

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,9 @@ import { Bot, MemoryStorage, BotStateManager, Middleware } from 'botbuilder';
 import 'isomorphic-fetch';
 // import { BotFrameworkAdapter } from 'botbuilder-services';
 // import { createServer } from 'restify';
-import { Topic } from './topics';
-import { simpleForm, SimpleFormSchema } from './forms';
+import { Topic } from 'botbuilder-topical';
+import { simpleForm, SimpleFormSchema } from 'botbuilder-topical';
+import { stringPrompt } from 'botbuilder-topical';
 
 const adapter = new ConsoleAdapter();
 
@@ -15,6 +16,15 @@ const bot = new Bot(adapter);
 bot
     .use(new MemoryStorage())
     .use(new BotStateManager())
+    .use({
+        async receiveActivity(context, next) {
+            context.reply("<<<");
+            // if (context.state.conversation.topical)
+            //     console.log(context.state.conversation.topical.instances);
+            await next();
+            context.reply(">>>")
+        }
+    })
     .onReceive(async context => {
         await Topic.do(context, () => alarmBot.createInstance(context));
     });
@@ -40,11 +50,73 @@ let alarmSchema: SimpleFormSchema = {
     }
 }
 
-const showAlarms = new Topic<undefined, { alarms: Alarm[] }>('showAlarms')
+interface ShowAlarmInitArgs {
+    alarms: Alarm[]
+}
+
+const showAlarms = new Topic<any, ShowAlarmInitArgs>('showAlarms')
     .init((context, topic) => {
         context.reply(`You have the following alarms set:`);
-        topic.args.alarms.forEach(alarm => context.reply(`${alarm.name} for ${alarm.when}`));
+        topic.args.alarms.forEach(alarm => context.reply(`"${alarm.name}" for ${alarm.when}`));
         topic.complete();
+    });
+
+interface DeleteAlarmInitArgs {
+    alarms: Alarm[];
+}
+
+interface DeleteAlarmState {
+    alarms: Alarm[];
+    alarmName: string;
+    confirm: boolean;
+    child: string;
+}
+
+interface DeleteAlarmCallbackArgs {
+    alarmName: string;
+}
+
+const deleteAlarm = new Topic<DeleteAlarmState, DeleteAlarmInitArgs, DeleteAlarmCallbackArgs>('deleteAlarm')
+    .init(async (context, topic) => {
+        if (topic.args.alarms.length === 0) {
+            context.reply(`You don't have any alarms.`);
+            topic.complete();
+            return;
+        }
+
+        topic.instance.state.alarms = topic.args.alarms;
+
+        const names = topic.args.alarms
+            .map(alarm => alarm.name)
+            .join(', ');
+
+        topic.instance.state.child = await stringPrompt.createInstance(context, topic.instance.name, {
+            name: 'whichAlarm',
+            prompt: `Which alarm do you want to delete? (${names})`,
+        });
+    })
+    .onReceive(async (context, topic) => {
+        if (topic.instance.state.child)
+            await Topic.dispatch(context, topic.instance.state.child);
+    })
+    .onComplete(stringPrompt, async (context, topic) => {
+        switch (topic.args.name) {
+            case 'whichAlarm':
+                topic.instance.state.alarmName = topic.args.value;
+                topic.instance.state.child = await stringPrompt.createInstance(context, topic.instance.name, {
+                    name: 'confirm',
+                    prompt: `Are you sure you want to delete alarm "${topic.args.value}"? (yes/no)"`,
+                });
+                break;
+            case 'confirm':
+                topic.complete(topic.args.value === 'yes'
+                    ? {
+                        alarmName: topic.instance.state.alarmName
+                    }
+                    : undefined
+                )
+                break;
+        }
     });
 
 interface AlarmBotState {
@@ -60,29 +132,42 @@ const alarmBot = new Topic<AlarmBotState>('alarmBot')
     .onReceive(async (context, topic) => {
         if (topic.instance.state.child)
             return Topic.dispatch(context, topic.instance.state.child);
-        else if (context.request.type === 'message') {
-            if (context.request.text === "set alarm") {
-                topic.instance.state.child = await simpleForm.createInstance(
-                    context,
-                    {
-                        schema: alarmSchema,
-                    },
-                    topic.instance.name,
-                );
-            } else if (context.request.text === "show alarms") {
+
+        if (context.request.type === 'message') {
+            if (/set/i.test(context.request.text)) {
+                topic.instance.state.child = await simpleForm.createInstance(context, topic.instance.name, {
+                    schema: alarmSchema,
+                });
+            } else if (/show/i.test(context.request.text)) {
                 topic.instance.state.child = await showAlarms.createInstance(context, {
                     alarms: topic.instance.state.alarms
                 });
-            } else {
-                context.reply(`I didn't understand that.`);
+            } else if (/delete/i.test(context.request.text)) {
+                topic.instance.state.child = await deleteAlarm.createInstance(context, topic.instance.name, {
+                    alarms: topic.instance.state.alarms
+                });
             }
         }
     })
-    .onComplete(simpleForm, async (context, topic) => {
+    .onComplete(simpleForm, (context, topic) => {
         topic.instance.state.alarms.push({
             name: topic.args.form['name'],
             when: topic.args.form['when'],
         });
+
         context.reply(`Alarm successfully added!`);
+        
         topic.instance.state.child = undefined;
-    });
+    })
+    .onComplete(deleteAlarm, (context, topic) => {
+        if (topic.args) {
+            topic.instance.state.alarms = topic.instance.state.alarms
+                .filter(alarm => alarm.name !== topic.args.alarmName);
+
+            context.reply(`Alarm "${topic.args.alarmName}" has been deleted.`)
+        } else {
+            context.reply(`Okay, the status quo has been preserved.`)
+        }
+
+        topic.instance.state.child = undefined;
+    })
